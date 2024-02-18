6f76370c9aeb318e7c4f0ecdf49385bba98cce9a
dolakzdenek <58859634+dolakzdenek@users.noreply.github.com>
2024-02-18 14:55:13 +0100

    new flags

--- src/screen/Landing.tsx
+++ src/screen/Landing.tsx
@@ -1,6 +1,6 @@
 import { useNavigation } from '@react-navigation/native'
 import React from 'react'
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import { MMKV } from 'react-native-mmkv'
 import { MainBox } from '../components/MainBox'
 import { MainText } from '../components/MainText'
@@ -12,6 +12,9 @@ export const storage = new MMKV()
 const Landing = () => {
   const [name, setName] = React.useState<string>(storage.getString('user.name') || '')
   const navigation = useNavigation()
+  const flag = require('../../assets/images/flags/h240/ug.png')
+
+  console.log('name', flag)
 
   const startQuiz = () => {
     console.log('Quiz Started', name)
@@ -22,18 +25,26 @@ const Landing = () => {
   return (
     <>
       <MainBox>
-        <MainText
-          variant="header"
-        >
-          Football Quiz
-        </MainText>
-        <Input
-
-          value={name}
-          onChangeText={setName}
-          placeholder="Enter your name"
-        />
-        <Button label="Start Quiz" onPress={startQuiz} />
+        <View style={{
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}>
+          <MainText
+            variant="header"
+          >
+            Football Quiz
+          </MainText>
+          <Input
+            value={name}
+            onChangeText={setName}
+            placeholder="Enter your name"
+          />
+          <View style={{ width: 150, height: 150, borderRadius: 150, overflow: "hidden", backgroundColor: 'white' }}>
+            <Image source={flag} resizeMode="contain" />
+          </View>
+          <Button label="Start Quiz" onPress={startQuiz} />
+        </View>
       </MainBox>
     </>
   )
